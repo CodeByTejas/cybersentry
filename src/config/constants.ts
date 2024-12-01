22bc40fee1225474259e6d206c4476f5363deffc
Tejas Gupta <68655741+CodeByTejas@users.noreply.github.com>
2024-12-01 12:56:00 +0530

    Update constants.ts

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-  ABUSEIPDB_API_KEY: '74e9a117b7f84b2c5d06c75243659608895b07ef74caa9a89eeb1db429ccd2b0a70323adb114a230',
+  ABUSEIPDB_API_KEY: 'enter your api key',
   ABUSEIPDB_BASE_URL: 'https://api.abuseipdb.com/api/v2',
   SYSTEM_METRICS_WS_URL: 'wss://demo.piesocket.com/v3/channel_123?api_key=VCXCEuvhGcBDP7XhiJJUDvR1e1D3eiVjgZ9VRiaV&notify_self'
 };
